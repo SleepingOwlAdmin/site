15ac526c9dc1a1ab0872ac2feeb3fad4eb10159c
Butschster <butschster@gmail.com>
2016-11-17 01:56:26 +0300

    Add english locale

--- resources/lang/en/site.php
+++ resources/lang/en/site.php
@@ -1,5 +1,18 @@
 <?php
 
 return [
-
+    'title' => 'SleepingOwl - Administrative interface builder for Laravel.',
+    'menu' => [
+        'docs' => 'Docs',
+        'demo' => 'Demo',
+        'home' => 'Home'
+    ],
+    'search' => [
+        'placeholder' => 'Search on documentation',
+        'no_results' => 'We didn\'t find any result for "{{query}}". Sorry!'
+    ],
+    'error' => [
+        '404' => 'You seem to have upset the delicate internal balance of my housekeeper.',
+        '503' => 'Be right back.'
+    ]
 ];
